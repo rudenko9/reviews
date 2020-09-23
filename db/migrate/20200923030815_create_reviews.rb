2cfb759138d73bf41ccf5fcf07f214cb825d4091
github username <github email address>
2020-09-22 23:35:43 -0400

    create models

---
+++ db/migrate/20200923030815_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.string :name
+      t.string :model
+      t.string :brand
+      t.string :storage
+      t.integer :price
+      t.string :data
+
+      t.timestamps
+    end
+  end
+end
